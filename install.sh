c4e6a13b54741ef35a06591bc0305816cd621d16
Marina18 <marina@ersilia.io>
2026-08-06 16:28:54 +0200

    pinned rdkit and chemprop dependencies

---
+++ install.sh
@@ -0,0 +1,4 @@
+/home/marina/anaconda3/envs/eos1lb5/bin/python -m pip install lazyqsar[descriptors]==2.3.0
+/home/marina/anaconda3/envs/eos1lb5/bin/python -m pip install chemprop==2.2.0
+lazyqsar-setup
+/home/marina/anaconda3/envs/eos1lb5/bin/python -m pip install rdkit==2025.9.1
